f6bbda1bf18345cdd6cd471033971ed22d6dcef9
nao <chiwoo@postech.ac.kr>
2024-06-24 10:55:26 +0700

    test: add server

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Minja</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('loader.css') }}" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite(['resources/ts/main.ts'])
 </head>
 
